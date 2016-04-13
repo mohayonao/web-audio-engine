63908b109738f7b9f74c82a946520be7c461aa08
mohayonao <mohayonao@gmail.com>
2016-04-14 06:03:47 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ const OfflineAudioContext = require("./context/OfflineAudioContext");
 const StreamAudioContext = require("./context/StreamAudioContext");
 const RenderingAudioContext = require("./context/RenderingAudioContext");
 const WebAudioContext = require("./context/WebAudioContext");
+const api = require("./api");
+const impl = require("./impl");
+const decoder = require("./decoder");
+const encoder = require("./encoder");
 
 module.exports = {
   OfflineAudioContext,
   StreamAudioContext,
   RenderingAudioContext,
-  WebAudioContext
+  WebAudioContext,
+  api, impl, decoder, encoder
 };
