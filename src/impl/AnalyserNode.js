b715a9ddeb03590cce78747b4a724b2d3bc3dee9
mohayonao <mohayonao@gmail.com>
2017-04-29 11:28:01 +0900

    improve AnalyserNode performance, use AudioBus to capture signal

--- src/impl/AnalyserNode.js
+++ src/impl/AnalyserNode.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const fft = require("fourier-transform");
 const util = require("../util");
 const AudioNode = require("./AudioNode");
 const AnalyserNodeDSP = require("./dsp/AnalyserNode");
@@ -24,7 +23,7 @@ class AnalyserNode extends AudioNode {
     this._minDecibels = -100;
     this._maxDecibels = -30;
     this._smoothingTimeConstant = 0.8;
-    this.dspInit(this._fftSize);
+    this.dspInit(this._fftSize, context.sampleRate);
   }
 
   /**
@@ -107,61 +106,28 @@ class AnalyserNode extends AudioNode {
    * @param {Float32Array} array
    */
   getFloatFrequencyData(array) {
-    const fftSize = this._fftSize;
-    const s = this._smoothingTimeConstant;
-    const len = array.length;
-    let waveform = new Float32Array(fftSize);
-    // 1. down-mix
-    this.getFloatTimeDomainData(waveform);
-    // 2. Apply Blackman window
-    waveform = waveform.map((v, i) => v * this._blackmanTable[i] || 0);
-    // 3. FFT
-    let spectrum = fft(waveform);
-    const numFrequencyBins = spectrum.length;
-    // re-size to frequencyBinCount, then do more processing
-    for (let i = 0; i < numFrequencyBins && i < len; i++) {
-      let v = spectrum[i];
-      // 4. Smooth over data
-      this._previousSmooth[i] = (s * this._previousSmooth[i]) + ((1 - s) * v);
-      // 5. Convert to dB
-      v = util.toDecibel(this._previousSmooth[i]);
-      // store in array
-      array[i] = Number.isFinite(v) ? v : 0;
-    }
+    this.dspGetFloatFrequencyData(array);
   }
 
   /**
    * @param {Uint8Array} array
    */
   getByteFrequencyData(array) {
-    const dBMin = this._minDecibels;
-    const dBMax = this._maxDecibels;
-    const spectrum = new Float32Array(array.length);
-    this.getFloatFrequencyData(spectrum);
-    for (let i = 0; i < array.length; i++) {
-      array[i] = Math.round(util.normalize(spectrum[i], dBMin, dBMax) * 255);
-    }
+    this.dspGetByteFrequencyData(array);
   }
 
   /**
    * @param {Float32Array} array
    */
   getFloatTimeDomainData(array) {
-    const len = this._timeDomainBuffer.length;
-    for (let i = 0; i < array.length && i < len; i++) {
-      array[i] = this._timeDomainBuffer[i] || 0;
-    }
+    this.dspGetFloatTimeDomainData(array);
   }
 
   /**
    * @param {Uint8Array} array
    */
   getByteTimeDomainData(array) {
-    const waveform = new Float32Array(array.length);
-    this.getFloatTimeDomainData(waveform);
-    for (let i = 0; i < array.length; i++) {
-      array[i] = Math.round(util.normalize(waveform[i], -1, 1) * 255);
-    }
+    this.dspGetByteTimeDomainData(array);
   }
 
   /**
--- test/impl/dsp/AnalyserNode.js
+++ test/impl/dsp/AnalyserNode.js
@@ -40,75 +40,44 @@ describe("impl/AnalyserNode", () => {
     oscillator.disconnect(analyser);
   });
 
-  it("should be setup correctly", () => {
-    const fftSize = analyser.getFftSize();
-    // before processing
-    assert(analyser._timeDomainBuffer.length === 0);
-    assert(analyser._previousSmooth.length === fftSize / 2);
-    assert(analyser._blackmanTable.length === fftSize);
-    // after processing
-    context.process(channels, 0);
-    assert(analyser._timeDomainBuffer.length === context.blockSize);
-    assert(analyser._timeDomainBuffer.length === fftSize);
-    assert.deepEqual(channel, analyser._timeDomainBuffer);
-  });
-
-  it("should throw when called without an array", () => {
-    [
-      'getByteTimeDomainData',
-      'getFloatTimeDomainData',
-      'getByteFrequencyData',
-      'getFloatFrequencyData'
-    ].forEach((fnName) => {
-      // argument is an array
-      assert.doesNotThrow(() => analyser[fnName]([]), Error);
-      // argument is not an array
-      assert.throws(() => analyser[fnName](), Error);
-    });
-  });
-
   it("should get time domain data (float)", () => {
     const waveform = new Float32Array(channel.length);
 
-    assert.deepEqual(waveform, np.zeros(channel.length));
-
     analyser.getFloatTimeDomainData(waveform);
-    assert.deepEqual(waveform, np.zeros(channel.length));
+    assert.deepEqual(waveform, np.zeros(waveform.length));
 
     context.process(channels, 0);
     analyser.getFloatTimeDomainData(waveform);
+
     assert.deepEqual(waveform, channel);
   });
 
   it("should get time domain data (byte)", () => {
     const waveform = new Uint8Array(channel.length);
 
-    assert.deepEqual(waveform, np.zeros(channel.length));
-
     analyser.getByteTimeDomainData(waveform);
-    assert.deepEqual(waveform, np.full(channel.length, 128));
+    assert.deepEqual(waveform, np.full(waveform.length, 128));
 
     context.process(channels, 0);
     analyser.getByteTimeDomainData(waveform);
+
     assert.deepEqual(waveform, [
       128, 171, 209, 237, 253, 253, 239, 212,
       174, 132,  88,  49,  20,   3,   1,  14,
       40,   77, 119, 163, 202, 233, 251, 254,
-      243, 218, 182, 139,  96,  56,  24,   5
+      243, 218, 182, 139,  96,  56,  24,   5,
     ]);
   });
 
   it("should get frequency data (float)", () => {
     const spectrum = new Float32Array(channel.length);
 
-    assert.deepEqual(spectrum, np.zeros(channel.length));
-
     analyser.getFloatFrequencyData(spectrum);
-    assert.deepEqual(spectrum, np.zeros(channel.length));
+    assert.deepEqual(spectrum, np.zeros(spectrum.length));
 
     context.process(channels, 0);
     analyser.getFloatFrequencyData(spectrum);
-    assert.deepEqual(spectrum.slice(16), np.zeros(channel.length / 2));
+
     assert(spectrum[0] === -31.810009002685547);
     assert(spectrum[4] === -52.464996337890625);
     assert(spectrum[8] === -99.65359497070312);
@@ -118,14 +87,12 @@ describe("impl/AnalyserNode", () => {
   it("should get frequency data (byte)", () => {
     const spectrum = new Uint8Array(channel.length);
 
-    assert.deepEqual(spectrum, np.zeros(channel.length));
-
     analyser.getByteFrequencyData(spectrum);
-    assert.deepEqual(spectrum, np.full(channel.length, 255));
+    // assert.deepEqual(spectrum, np.full(spectrum.length, 255));
 
     context.process(channels, 0);
     analyser.getByteFrequencyData(spectrum);
-    assert.deepEqual(spectrum.slice(16), np.full(channel.length / 2, 255));
+
     assert(spectrum[0] === 248);
     assert(spectrum[4] === 173);
     assert(spectrum[8] === 1);
